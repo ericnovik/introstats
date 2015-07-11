7bec8430d8e6bfbff3b8a38de8e48119bd4bd558
Eric Novik <eric.novik@gmail.com>
2015-07-11 19:23:30 -0400

    Initial version

--- R/hello.R
+++ R/hello.R
@@ -22,6 +22,14 @@ ggplot_it <- function(n = 100) {
 
 #table_1_1 <- read.csv("~/Google Drive/Statistics with R Book/prod/01-chapter/table_1-1.csv", stringsAsFactors = FALSE)
 #table_1_4 <- read.csv("~/Google Drive/Statistics with R Book/prod/01-chapter/table_1-4.csv", stringsAsFactors = FALSE)
-
 #devtools::use_data(table_1_4, table_1_1)
+
+#tv_hours <- read.csv("~/Documents/Rfiles/tvhrs.csv", stringsAsFactors = FALSE)
+#devtools::use_data(tv_hours)
+
+#students <- read.csv("~/Documents/Rfiles/student.csv", stringsAsFactors = FALSE)
+#devtools::use_data(students)
+
+# poverty <- read.csv("~/Documents/Rfiles/USdata.csv", stringsAsFactors = FALSE)
+# devtools::use_data(poverty)
 
